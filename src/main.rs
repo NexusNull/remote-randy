a1c3e825a97404cd09af252daded536d90d2f47d
NexusNull <p.wellershaus@googlemail.com>
2025-12-31 21:15:47 +0100

    update socket file perms

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
 use std::fs;
 use std::io::Read;
-use std::os::unix::net::{UnixListener, UnixStream};
+use std::os::unix::fs::PermissionsExt;
+use std::os::unix::net::{UnixListener};
 use std::process::Command;
 
 fn main() {
-    let msg = match fs::remove_file("/run/randy.sock") {
+    let socket_path = "/run/randy.sock";
+    let msg = match fs::remove_file(socket_path) {
         Ok(()) => "is fine",
-        Err(_NotExist) => "does not exist",
-        Err(_IsDir) => "is a directory",
-        Err(_) => "is not yours",
+        Err(_not_exist) => "does not exist",
     };
     println!("The file {}!", msg);
 
-    let listener = UnixListener::bind("/run/randy.sock").unwrap();
+    let listener = UnixListener::bind(socket_path).unwrap();
 
+    let mut perms = fs::metadata(socket_path).unwrap().permissions();
+    perms.set_mode(0o772);
+    fs::set_permissions(socket_path, perms).unwrap();
 
     for stream in listener.incoming() {
         let mut stream = stream.unwrap();
@@ -21,10 +24,17 @@ fn main() {
         let read_amount = stream.read(&mut buffer).unwrap();
         let request = std::str::from_utf8(&buffer[..read_amount]).unwrap();
         println!("{}", request);
-        if(request.starts_with("nexus")){
+        if request.starts_with("enable") {
             Command::new("/bin/bash")
                 .arg("-c")
-                .arg("touch /home/nexus/works")
+                .arg("/opt/randy/enable.sh")
+                .output()
+                .expect("failed to execute process");
+        }
+        if request.starts_with("disable") {
+            Command::new("/bin/bash")
+                .arg("-c")
+                .arg("/opt/randy/disable.sh")
                 .output()
                 .expect("failed to execute process");
         }
